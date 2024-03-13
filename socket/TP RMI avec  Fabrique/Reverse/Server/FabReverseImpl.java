a18c06d4d91179ef42a6bb2668f6fbb30fcc79cb
Rayen Slouma <rayen.slouma@etudiant-fst.utm.tn>
2024-03-13 23:32:11 +0100

    update

--- socket/TP RMI avec  Fabrique/Reverse/Server/FabReverseImpl.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-import java.rmi.server.*;
-public class FabReverseImpl extends UnicastRemoteObject implements
-FabReverseInterface {
-public FabReverseImpl() throws RemoteException {};
-public ReverseInterface newReverse() throws RemoteException {
-return new Reverse(); } }
--- socket/TP RMI avec  Fabrique/Reverse/Server/FabReverseInterface.java
+++
@@ -1,3 +0,0 @@
-import java.rmi.*;
-public interface FabReverseInterface extends Remote {
-public ReverseInterface newReverse() throws RemoteException;}
